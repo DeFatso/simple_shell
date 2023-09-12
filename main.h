98afa403509276171bcddbd05e0a0c215245b353
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-09-12 09:40:19 +0000

    readme added

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*headerfiles*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/*prototypes*/
+
+
+#endif
