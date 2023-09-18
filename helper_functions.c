e225a6406e8842322d01b2f8a2ca6a16df2709b9
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-09-18 14:50:57 +0000

    more custom functions

--- helper_functions.c
+++ helper_functions.c
@@ -1,5 +1,27 @@
 #include "shell.h"
 /**
+ * _strcat - concatenates two strings
+ * @dest: string to concate with src
+ * @src: string to concate to dest
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*dest++)
+		;
+	dest--;
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (p);
+}
+/**
  * _strlen - custom made strlen
  * @str: parameter-> string
  * Return: length of str
---
+++ helper_functions1.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncmp - custom strncmp
+ * @s1: string 1
+ * @s2: string 2
+ * @n: he maximum number of characters to compare.
+ * Return: returns 0 if strings are equal upto n
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	while (n > 0 && *s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+
+	if (n == 0)
+	{
+		return 0;
+	}
+	else
+	{
+
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	}
+}
