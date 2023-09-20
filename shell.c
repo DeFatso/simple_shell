7fcdb74b929824450ad0feac96b6ee3b6b3763af
Charis <charisadu@gmail.com>
2023-09-20 10:53:54 +0000

    resolve std err

--- shell.c
+++ shell.c
@@ -45,8 +45,10 @@ int main(int ac, char *av[])
 		{
 			write(2, av[0], _strlen(av[0]));
 			write(2, ": 1: ", 5);
-			write(2, line, _strlen(line));
+			write(2, cmd_vector[0], _strlen(cmd_vector[0]));
 			write(2, " not found\n", 11);
+			if (pipe == 0)
+				exit(127);
 			continue;
 		}
 
