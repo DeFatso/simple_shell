843dee136dbef22dfeaca2bfa0a1d1ab373414c9
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-09-20 22:10:06 +0000

    new prototypes

--- shell.h
+++ shell.h
@@ -29,4 +29,10 @@ int _strcmp(const char *str1, const char *str2);
 char *_strchr(const char *str, int character);
 char *_strtok(char *str, const char *delim);
 
+/*new prototypes*/
+void handle_command_not_found(const char *cmd);
+void execute_command(const char *cmd_path, char *const cmd_vector[]);
+void process_input(char *input);
+void run_shell(void);
+
 #endif /* SHELL_H */
